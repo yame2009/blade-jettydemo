7d40d2c4d1a440e86ec0ae6c88a81e1f7052725a
biezhi <biezhi.me@gmail.com>
2015-06-09 18:19:35 +0800

    - 增加模板引擎支持

--- src/main/java/blade/sample/App.java
+++ src/main/java/blade/sample/App.java
@@ -2,6 +2,7 @@ package blade.sample;
 
 import blade.Blade;
 import blade.BladeApplication;
+import blade.render.JetbrickRender;
 import blade.server.BladeServer;
 
 public class App implements BladeApplication{
@@ -12,9 +13,11 @@ public class App implements BladeApplication{
 		// 设置路由、拦截器包所在包
 		Blade.defaultRoute("blade.sample");
 		
-		Blade.view("/views/", ".jsp");
+		Blade.view("/views/", ".html");
 		
 		Blade.staticFolder("/res");
+		
+		Blade.viewEngin(new JetbrickRender());
 	}
 	
 	public static void main(String[] args) {
