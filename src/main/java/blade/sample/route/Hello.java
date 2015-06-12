508e7ce3e6d6d4f6e383caf696efc318dfe27c17
biezhi <biezhi.me@gmail.com>
2015-06-12 18:12:16 +0800

    -

--- src/main/java/blade/sample/route/Hello.java
+++ src/main/java/blade/sample/route/Hello.java
@@ -2,14 +2,16 @@ package blade.sample.route;
 
 import blade.annotation.Path;
 import blade.annotation.Route;
+import blade.wrapper.Request;
 
 @Path
 public class Hello {
 
 	
-	@Route("/hello")
-	public String hello() {
+	@Route("/hello/:name")
+	public String hello(Request request) {
 		System.out.println("hello");
+		System.out.println("name = " + request.pathParam(":name"));
 		return "hello";
 	}
 	
